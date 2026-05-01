a3bcc5e9e5b57d9e56b9116591bb0af5aa0d69f7
Reuben-jeffery <reubenjeffery47@gmail.com>
2026-05-01 11:44:37 +0100

    made some corrections on the settings

--- TogaProject/settings.py
+++ TogaProject/settings.py
@@ -131,6 +131,7 @@ DATE_FORMAT = "d M Y"
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
